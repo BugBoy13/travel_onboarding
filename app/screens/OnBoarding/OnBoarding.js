c5ddf02511ead3756a6196e2f96c2b02f3a2dbf6
vardaan <vardaan13121997@gmail.com>
2021-06-21 23:03:01 +0530

    Design Config done

---
+++ app/screens/OnBoarding/OnBoarding.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+const OnBoarding = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>
+                OnBoarding Screen
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default OnBoarding;
